030f11dce0d9adf18979e597595e25f17a12bcce
6hiro <hiro6github@gmail.com>
2022-04-11 07:39:02 +0900

    first commit

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -30,7 +30,8 @@ urlpatterns = [
     path('forgot/', accounts_views.ForgotAPIView.as_view(), name='forgot-password'),
     path('reset-password/', accounts_views.ResetAPIView.as_view(),
          name='reset-password'),
-    #     path('user/', accounts_views.UserAPIView.as_view(), name='user'),
+
+    path('user/', accounts_views.UserAPIView.as_view(), name='user'),
     path('update-account/<uuid:pk>/',
          accounts_views.UpdateUserAPIView.as_view(), name="updare-user"),
     path('delete-account/<uuid:pk>/',
